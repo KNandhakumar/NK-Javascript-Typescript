f33104353a119eecf49a8b08ccc79d39b0ad1f9b
Nandhakumar <nknandhakumar64@gmail.com>
2024-12-04 16:28:20 +0530

    MN-2026

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("hello world");
+class My{
+    name = "MN"; // this called class fields
+    age = 23;
+    passion = "Actress";
+    myLove = "Nk Motivation MN"
+    // creatong a function in class
+    myDream(){
+        console.log("Nk💙 MN")
+        console.log(this.myLove)
+    }
+}
+
+// creating a object for My class
+let my = new My();
+// accessing the class flieds in outer class
+console.log(my.name)
+console.log(my.age);
+console.log(my.passion)
+my.myDream();
